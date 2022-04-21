cd4f5bbe6d54fad2db5bc9ba936827f8fe0bc6d2
WaltVenditi <waltvenditti@gmail.com>
2022-04-21 10:04:00 -0400

    added version of node to package.json and added dev_db_url variable for deployment to Heroku

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://waltvenditti:local-lib-db@clusterll.dqcn2.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://waltvenditti:local-lib-db@clusterll.dqcn2.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
