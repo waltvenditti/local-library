ef76997f46c2c7cf1273fd65e6ac0f5d785d5a18
WaltVenditi <waltvenditti@gmail.com>
2022-04-21 09:58:04 -0400

    installed compression and helmet middleware

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var compression = require('compression');
+var helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +23,9 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(helmet());
+app.use(compression());
 
 app.use(logger('dev'));
 app.use(express.json());
